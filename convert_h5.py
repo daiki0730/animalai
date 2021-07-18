6a35e5519dd26e85607cb2fb704ba3cef3807bef
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-07-18 23:41:43 +0900

    coreMLToolsの変換と実行

--- convert_h5.py
+++ convert_h5.py
@@ -1,15 +1,11 @@
 import coremltools
 from keras.models import load_model
 
-coreml_model =
-coremltools.converters.keras.convert('animal_cnn_aug.h5',
+coreml_model = coremltools.converters.keras.convert(
+'animal_cnn_aug.h5',
 input_names='image',
 image_input_names='image',
 output_names='Prediction',
-class_labels=[
-'monkey',
-'boar',
-'crow'
-],)
+class_labels=['monkey','boar','crow'],)
 
 coreml_model.save('/Users/daiki/animalai/animal_cnn_aug.mlmodel')

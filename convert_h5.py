4294b2807e7a1e920f4a01fbbed35be29e5382f2
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-07-21 01:05:10 +0900

    修正

--- convert_h5.py
+++ convert_h5.py
@@ -7,5 +7,4 @@ input_names='image',
 image_input_names='image',
 output_names='Prediction',
 class_labels=['monkey','boar','crow'],)
-
 coreml_model.save('/Users/daiki/animalai/animal_cnn_aug.mlmodel')
